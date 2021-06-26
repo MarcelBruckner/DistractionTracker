15a75bd104977ed02f9f2d5acd5b6d385a67a7f4
brucknem <brucknem>
2021-06-26 20:27:59 +0200

    Built add entry layout

--- app/src/main/java/org/brucknem/distractiontracker/AddEntryActivity.kt
+++ app/src/main/java/org/brucknem/distractiontracker/AddEntryActivity.kt
@@ -6,16 +6,19 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat
+import android.util.Log
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.*
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
 
 import java.util.Calendar;
 
 class AddEntryActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
     TimePickerDialog.OnTimeSetListener {
-    
+
     lateinit var textView: TextView
     lateinit var button: Button
     var day = 0
@@ -28,15 +31,30 @@ class AddEntryActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener
     var myYear: Int = 0
     var myHour: Int = 0
     var myMinute: Int = 0
+    lateinit var calendar: Calendar
+
+    private var dateFormat: java.text.DateFormat = SimpleDateFormat.getDateTimeInstance();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: called")
+
         setContentView(R.layout.activity_add_entry)
-        title = "KotlinApp"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        calendar = Calendar.getInstance()
+
         textView = findViewById(R.id.textView)
         button = findViewById(R.id.btnPick)
+
+        myDay = calendar.get(Calendar.DAY_OF_MONTH)
+        myMonth = calendar.get(Calendar.MONTH)
+        myYear = calendar.get(Calendar.YEAR)
+        myHour = calendar.get(Calendar.HOUR) + 12 * calendar.get(Calendar.AM_PM)
+
+        myMinute = calendar.get(Calendar.MINUTE)
+        setDateTime()
+
         button.setOnClickListener {
-            val calendar: Calendar = Calendar.getInstance()
             day = calendar.get(Calendar.DAY_OF_MONTH)
             month = calendar.get(Calendar.MONTH)
             year = calendar.get(Calendar.YEAR)
@@ -51,7 +69,7 @@ class AddEntryActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener
         myYear = year
         myMonth = month
         val calendar: Calendar = Calendar.getInstance()
-        hour = calendar.get(Calendar.HOUR)
+        hour = calendar.get(Calendar.HOUR) + 12 * calendar.get(Calendar.AM_PM)
         minute = calendar.get(Calendar.MINUTE)
         val timePickerDialog = TimePickerDialog(
             this, this, hour, minute,
@@ -63,7 +81,21 @@ class AddEntryActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener
     override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
         myHour = hourOfDay
         myMinute = minute
-        textView.text =
-            "Year: $myYear\nMonth: $myMonth\nDay: $myDay\nHour: $myHour\nMinute: $myMinute"
+        setDateTime()
+    }
+
+    private fun setDateTime() {
+        calendar.set(myYear, myMonth, myDay, myHour, myMinute)
+
+        textView.text = dateFormat.format(calendar.timeInMillis)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
+    }
+
+    companion object {
+        private const val TAG = "AddEntryActivity"
     }
 }
